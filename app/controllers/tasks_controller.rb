6c959f996bb85a93e6ef8d4b1f3d15244fe641af
Rodrigo Riveros <docentedigitalarica@gmail.com>
2021-07-22 17:13:02 -0400

    Tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,39 @@
 # task_controller.rb
 class TasksController < ApplicationController
   before_action :find_task, only: [:id]
+
   def index
-    @tasks = Task.all
+    @tasks = Task.all # Mostrar todas las tareas
   end
 
-  def find_task
-    @tasks = Task.find(params[:id])
+  def show
+    @tasks = Task.find(params[:id]) # Mostrar una tarea
+  end
+
+  def new
+    @task = Task.new # Crear una nueva tarea
+  end
+
+  def create
+    @task = Task.new(params[:task])
+    @task.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task]) # Actualizar una tarea
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy # Destruir una tarea
+
+    # no need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path
   end
 end
